7b61b1ce2cc42e39cf65a275a9eea915f7699bc6
Henry Cook <henrycook@Henrys-Air.lan>
2020-09-20 11:06:36 -0400

    Added 4 more questions

--- app/questions.js
+++ app/questions.js
@@ -1,17 +1,68 @@
-const OsmiumPulseThrusters = () => {
-  let dilithiumVacuum = "stable";
-  let warpNode = "maximum";
-  let quantumImpulser = "Blow Up Ship";
+// const OsmiumPulseThrusters = () => {
+//   let dilithiumVacuum = "stable";
+//   let warpNode = "maximum";
+//   let quantumImpulser = "Blow Up Ship";
 
-  if (dilithiumVacuum === "stable" && warpNode === "maximum") {
-    return "stabilized";
-  } else {
-    return quantumImpulser;
-  }
-};
+//   if (dilithiumVacuum === "stable" && warpNode === "maximum") {
+//     return "stabilized";
+//   } else {
+//     return quantumImpulser;
+//   }
+// };
 
-console.log(OsmiumPulseThrusters());
+// console.log(OsmiumPulseThrusters());
 
 // Answer = "stableized"
 
 // “I'm detecting a destabilization in the osmium pulse thrusters. It seems like the function below isn't returning what it should!”
+
+// Prompt => The beryllium neogenic is offline, use write complete the function below to bring it back online!
+
+// const status = "offline";
+
+// const berylliumNeogenic = () => {
+//   status = "online";
+//   return status;
+// };
+
+// berylliumNeogenic();
+
+//answer => "online"
+
+//The quantum bio-containers memorybank has been wiped clean! Write a function that returns the string "cerium phaser turbine"(exactly the way it's written!) if the bioContainers = true.
+
+// let bioContainers = true
+
+// const quantumMemoryBank = () => {
+
+// }
+
+// quantumMemoryBank()
+
+// answer => "cerium phaser turbine"
+
+// There's a series of nanowave pulse signatures thats overheating the electro-ceramic landing struts! The shielding must have gone down, fix it below!
+
+// const electroCeramicLandingStruts = (heatLevel) => {
+//   let sheild = "off"
+//   let strutsMaxHeat = 600
+
+//   if (strutsMaxHeat < heatLevel && sheild === "off") {
+//     return "Systems Overheated"
+//   } else {
+//     return "All Systems Normal"
+//   }
+
+// }
+
+// electroCeramicLandingStruts(1000)
+
+// We need a new fucntion that takes in an array and outputs the length, make sure the input is the correct datastructure! This should help regulate the delta-wave prorulsion unit. Create it, and hurry!
+
+// const deltaWaveRegulator = () => {
+
+// }
+
+// deltaWaveRegulator([1342, 3029, 5430, 22034, 9334,])
+
+// answer => "5"
