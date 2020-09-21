adb24e30cb9b7215be211d9d1b83d8bcb6307907
Henry Cook <henrycook@Henrys-Air.lan>
2020-09-21 16:41:00 -0400

    Fixed a few bugs and added some questions

--- app/questions.js
+++ app/questions.js
@@ -57,7 +57,7 @@
 
 // electroCeramicLandingStruts(1000)
 
-// We need a new fucntion that takes in an array and outputs the length, make sure the input is the correct datastructure! This should help regulate the delta-wave prorulsion unit. Create it, and hurry!
+// We need a new function that takes in an array and outputs the length, make sure the input is the correct datastructure! This should help regulate the delta-wave prorulsion unit. Create it, and hurry!
 
 // const deltaWaveRegulator = () => {
 
@@ -66,3 +66,37 @@
 // deltaWaveRegulator([1342, 3029, 5430, 22034, 9334,])
 
 // answer => "5"
+
+// Oh no! We need to degauss the zirconium gamma-wave brackets! We could blow up in seconds if we don't! See why the function below isn't returning [443, 858, 538, 367] like it should.
+
+const degaussInitiator = () => {
+  const solarSailCalibration = [223, 443, 858, 538, 367];
+  const rubidiumCollector = solarSailCalibration.splice(1);
+  return rubidiumCollector;
+};
+
+console.log(degaussInitiator().toString());
+
+// The rubidium capacitor formula is off, we should be getting 0 but itsted it's setting off the self destruct! Fix it quick!
+
+// const rubidiumCapacitorFormula = (crystalCore) => {
+//   let theraMagneticImpulser = 19338;
+//   let lithiumWrap = 32098;
+//   let quantumCoolRate;
+
+//   if (crystalCore > 2000) {
+//     quantumCoolRate = 19338;
+//     return theraMagneticImpulser - quantumCoolRate;
+//   } else {
+//     quantumCoolRate = 233;
+//     theraMagneticImpulser += quantumCoolRate;
+//   }
+
+//   if (theraMagneticImpulser === 19571) {
+//     return "Self Destruct";
+//   }
+// };
+
+// rubidiumCapacitorFormula(1239);
+
+// answer = "0"
